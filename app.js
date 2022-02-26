941664bbe4617799ff04e5cc382c9f5e8ddc289b
Ananias Raphael <ananias.nobrega@gmail.com>
2022-02-26 14:33:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// var mostrarSite = true;
+
+// const site = "www.meninonias.io";
+
+// console.log('Mostrando site...')
+// console.log('não pode esquecer o Hello World')
+
+// if(mostrarSite){
+//     console.log('site', site);
+// }
+
+var calculadora = require("./calculadora");
+
+console.log(calculadora.soma(10, 20))
